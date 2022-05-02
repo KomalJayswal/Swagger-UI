69df5f9136f76d09e943a0360b317ea279db76c0
Komal-Jayswal <Komal.Jayswal@maersk.com>
2022-05-03 04:48:22 +0530

    base structure

---
+++ src/main/java/net/learning/SwaggerUI/SwaggerUiApplication.java
@@ -0,0 +1,13 @@
+package net.learning.SwaggerUI;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SwaggerUiApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(SwaggerUiApplication.class, args);
+	}
+
+}
---
+++ src/test/java/net/learning/SwaggerUI/SwaggerUiApplicationTests.java
@@ -0,0 +1,13 @@
+package net.learning.SwaggerUI;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class SwaggerUiApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
