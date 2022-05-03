eaa6effd255eeb457854be141be04446deff96d2
Komal-Jayswal <Komal.Jayswal@maersk.com>
2022-05-03 20:40:54 +0530

    Swagger UI Integration

--- src/main/java/net/learning/SwaggerUI/swagger/SubmitDocumentContract.java
+++ src/main/java/net/learning/SwaggerUI/swagger/SubmitDocumentContract.java
@@ -8,14 +8,14 @@ import io.swagger.v3.oas.annotations.Operation;
 import io.swagger.v3.oas.annotations.media.Content;
 import io.swagger.v3.oas.annotations.media.Schema;
 import io.swagger.v3.oas.annotations.responses.ApiResponse;
-import net.learning.SwaggerUI.model.response.SuccessSampleResponse;
+import net.learning.SwaggerUI.model.response.*;
 
 @Operation(summary = "Submit import document", responses = {
         @ApiResponse(responseCode = "201", description = "Document Created successfully", content = @Content(schema = @Schema(implementation = SuccessSampleResponse.class))),
-        @ApiResponse(responseCode = "401", description = "Client_credentials authentication failed", content = @Content(schema = @Schema)),
-        @ApiResponse(responseCode = "403", description = "Unauthorized access", content = @Content(schema = @Schema)),
-        @ApiResponse(responseCode = "400", description = "Invalid input", content = @Content(schema = @Schema)),
-        @ApiResponse(responseCode = "500", description = "Internal server error", content = @Content(schema = @Schema)), })
+        @ApiResponse(responseCode = "401", description = "Client_credentials authentication failed", content = @Content(schema = @Schema(implementation = FailedAuthenticationSampleResponse.class))),
+        @ApiResponse(responseCode = "403", description = "Unauthorized access", content = @Content(schema = @Schema(implementation = UnauthorizedSampleResponse.class))),
+        @ApiResponse(responseCode = "400", description = "Invalid Input", content = @Content(schema = @Schema(implementation = BadRequestSampleResponse.class))),
+        @ApiResponse(responseCode = "500", description = "Internal server error", content = @Content(schema = @Schema(implementation = InternalServerErrorSampleResponse.class))), })
 @Retention(RetentionPolicy.RUNTIME)
 @Target(ElementType.METHOD)
 public @interface SubmitDocumentContract {
